ae580f7b1f36faef501a4d7d997c6865378adc6a
kazkt01 <91418688+kazkt01@users.noreply.github.com>
2024-11-29 20:53:12 +0900

    update

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,3 +1,4 @@
+// 初代
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
@@ -6,7 +7,9 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
+  // apiKey: "AIzaSyA_c5qaT1ugja-IE1abfXaNcRLGgFQwh3U",
   apiKey: "AIzaSyA_c5qaT1ugja-IE1abfXaNcRLGgFQwh3U",
+
   authDomain: "cotumo-beta-var3.firebaseapp.com",
   projectId: "cotumo-beta-var3",
   storageBucket: "cotumo-beta-var3.appspot.com",
@@ -20,3 +23,23 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app); // ←databaseと連携
 
 export { db };
+
+// 2代目
+
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyA_c5qaT1ugja-IE1abfXaNcRLGgFQwh3U",
+//   authDomain: "cotumo-beta-var3.firebaseapp.com",
+//   projectId: "cotumo-beta-var3",
+//   storageBucket: "cotumo-beta-var3.firebasestorage.app",
+//   messagingSenderId: "935804275341",
+//   appId: "1:935804275341:web:a226ebd25187a26fca70c4",
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
