aab78996b5a9a481e22e085b2d88cb26e463183d
kazkt01 <91418688+kazkt01@users.noreply.github.com>
2024-11-29 21:29:58 +0900

    update

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,4 +1,3 @@
-/*
 // 初代
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
@@ -14,33 +13,6 @@ const firebaseConfig = {
   storageBucket: "cotumo-beta-var3.appspot.com",
   messagingSenderId: "935804275341",
   appId: "1:935804275341:web:838355ca85b4bbb4ca70c4",
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-const db = getFirestore(app); // ←databaseと連携
-
-export { db };
-
-*/
-
-// cotumo-mode-pf-02
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAouk7zfVY8U6Gb5979qOTyc8DJfPQMgPA",
-  authDomain: "cotumo-mode-pf-02.firebaseapp.com",
-  projectId: "cotumo-mode-pf-02",
-  storageBucket: "cotumo-mode-pf-02.firebasestorage.app",
-  messagingSenderId: "664702013164",
-  appId: "1:664702013164:web:c6035dcc6830b355461fc9",
 };
 
 // Initialize Firebase
